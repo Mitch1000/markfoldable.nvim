47fb8a941ec2d9a500ba59b82042e2946b4021be
Mitch1000 <mwdrohan@gmail.com>
2025-07-24 17:20:15 -0400

    adds code to mark neovim folds

---
+++ lua/anchorage/init.lua
@@ -0,0 +1,38 @@
+local M = {}
+local vim = vim
+
+--- default config
+---@class BackpackConfig
+M.config = {
+  opened_icon = '',
+  closed_icon = '',
+  anchor_color = "#5f5f5f",
+  anchor_bg = "none",
+}
+
+local function check_config(config)
+    local err
+    return not err
+end
+
+--- update global configuration with user settings
+---@param config? BackpackConfig user configuration
+function M.setup(config)
+  if check_config(config) then
+      M.config = vim.tbl_deep_extend("force", M.config, config or {})
+  else
+      vim.notify("Anchorage: Errors found while loading user config. Using default config.", vim.log.levels.ERROR)
+  end
+
+  function AnchorageMarkFoldable()
+    require('anchorage.mark_foldable')()
+  end
+
+  local cmd = vim.cmd
+  cmd([[nnoremap z <cmd>execute "lua AnchorageMarkFoldable()"<CR>]])
+
+  cmd([[autocmd BufRead,BufNewFile, * execute "lua AnchorageMarkFoldable()"]])
+  cmd([[autocmd CursorMoved,CursorMovedI * execute "lua AnchorageMarkFoldable()"]])
+end
+
+return M
---
+++ lua/anchorage/insert_marker.lua
@@ -0,0 +1,33 @@
+local vim = vim
+
+local GetHighlightColor = require("helpers.get_highlight_color")
+-- 
+local background = GetHighlightColor('Normal', 'guibg')
+
+if (string.len(background) <= 0) then
+  background = 'NONE'
+end
+
+local function InsertMarker(lineNumber, char, col, position, ns_id)
+  vim.cmd([[hi AccordianMarker guifg=#5f5f5f guibg=]] .. background)
+
+  local api = vim.api
+  local bnr = vim.fn.bufnr('%')
+  local line_num = lineNumber
+  local col_num = col or 0
+
+  local opts = {
+    end_line = 0,
+    id = lineNumber +1,
+    virt_text = {{char, "AccordianMarker"}},
+    virt_text_pos = position,
+  }
+
+  if (position == 'overlay') then
+    opts.virt_text_win_col = col_num
+  end
+
+  return api.nvim_buf_set_extmark(bnr, ns_id, line_num, col_num, opts)
+end
+
+return InsertMarker
